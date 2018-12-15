fd0ed13188b98dce83d45f66652dae74e285c986
Negociation <tiagitofranca04@gmail.com>
2018-12-15 17:53:39 -0200

    Novo Branch

--- habboweb/16/11/web-gallery/js/registration.js
+++
@@ -1,9 +0,0 @@
-var pwErrors = [];
-var pwTotal = [];
-
-function showStatusMsg(field, msg, status) {
-	$(field + "-content").innerHTML = msg;
-	if (!Element.hasClassName($(field), "field-status-" + status)) {
-		$(field).className = "field-status-" + status;
-	}
-}
