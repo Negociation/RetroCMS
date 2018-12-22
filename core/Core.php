081d9d024d12f134491253285fc647a32e360055
Negociation <tiagitofranca04@gmail.com>
2018-12-22 01:50:56 -0200

    Fixed some issues about install

--- core/Core.php
+++ core/Core.php
@@ -13,7 +13,7 @@
 define( 'MYSQL_HOST', 'localhost' );
 define( 'MYSQL_USER', 'root' );
 define( 'MYSQL_PASSWORD', '' );
-define( 'MYSQL_DB_NAME', 'retroinstall' );
+define( 'MYSQL_DB_NAME', 'retrofinal' );
 
 //Check if MySQL Conection Works
 try{
