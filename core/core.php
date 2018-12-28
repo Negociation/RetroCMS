0555d4a8696d3cc38dd7797d549209af9b587590
Negociation <tiagitofranca04@gmail.com>
2018-12-28 19:58:02 -0200

    Core Diagnostic added :D

--- core/core.php
+++ core/core.php
@@ -21,16 +21,28 @@ foreach (glob("./core/packages/models/*.php") as $model){ require_once $model; }
 //Include All Controllers 
 foreach (glob("./Core/packages/controllers/*.php") as $controller){ include $controller; }
 
-
-//Check if MySQL Conection Works
-try{
-	//Recieve MySQL/MariaDB Conection
-	$hotelData = parse_ini_file('./core/install/settings.ini', true);
-	$hotelConection = new PDO('mysql:host=' . $hotelData['database']['host'] . ';dbname=' . $hotelData['database']['name'], $hotelData['database']['user'], $hotelData['database']['password']);
-}catch ( PDOException $e ){	
+if(version_compare(PHP_VERSION, '7.0.0', '<')){
+	$e = "<center> <br>Sua versão do PHP não é compativel com o RetroCMS, atualize antes de continuar! <br><br> Your version of PHP its not compatibile with RetroCMS, please update before install! </center>";
 	include './web/install/index.view';
 	exit;
+}else if(extension_loaded ("sodium")){
+	$e = "<center><br> Extensão sodim não encontrada <br> Extension Sodium disabled or not found</center>";
+	include './web/install/index.view';		
+	exit;
+}else{
+	//Check if MySQL Conection Works
+	try{
+		//Recieve MySQL/MariaDB Conection
+		$hotelData = parse_ini_file('./core/install/settings.ini', true);
+		$hotelConection = new PDO('mysql:host=' . $hotelData['database']['host'] . ';dbname=' . $hotelData['database']['name'], $hotelData['database']['user'], $hotelData['database']['password']);
+	}catch ( PDOException $e ){	
+		$e = "<center><br>Erro de conexão com o Banco de Dados<br>".$e->getMessage()."</center>";
+		include './web/install/index.view';
+		exit;
+	}
 }
+
+
 
 
 ?>
