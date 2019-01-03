37297af47e6bf9d2816fca780d2589f85469e3bd
Negociation <tiagitofranca04@gmail.com>
2019-01-03 18:25:47 -0200

    Added LanguageJS content again to remove warning

---
+++ web/includes/language/en.js
@@ -0,0 +1,156 @@
+//////////////////////////////////////////////////////////////
+//                       RetroCMS                           //
+//<<<<<<<<<<<<<< The Oldschool Era is Back >>>>>>>>>>>>>>>>>//
+//----------------------------------------------------------//
+// Developed by: Marcos ( M.tiago )                         //
+//////////////////////////////////////////////////////////////
+// Alpha Version 0.7.0 ( Opal )                             //
+//////////////////////////////////////////////////////////////
+
+var WORDS_EN = {
+
+
+  "Advertissement_Title" : "Advertissement",
+  "sign": "enter",
+  
+  
+	  //Topbar
+	  "topbar-count" : "members online",
+	  "topbar-status-loggedinBefore" : "Logged in as ", 
+	  "topbar-status-loggedinAfter" : "", 
+	  "topbar-status-notLogged" : "Not logged in", 
+	  
+	  
+	  //TopBar -> My Habbo Tab
+	  "topbar-myhabbo" : "My Habbo",
+	  "topbar-myhabbo-content-loggedinBefore" : "Welcome",
+	  "topbar-myhabbo-content-loggedinAfter" : "",
+	  "topbar-myhabbo-content-notLoggedin" : "Welcome! Please sign in or register",
+	  "topbar-myhabbo-inner-login" : "Sign In",
+	  "topbar-myhabbo-inner-register" : "Register Now, it's free!",
+	  "topbar-myhabbo-inner-client" : "Enter Habbo Hotel",
+	  "topbar-myhabbo-inner-home" : "View your Habbo Home Page",
+	  "topbar-myhabbo-inner-profile" : "Edit Your Settings",
+	  "topbar-myhabbo-inner-logout" : "Sign Out",
+	  
+
+	  //TopBar -> My Credits Tab
+	  "topbar-mycredits" : "My Coins",
+	  "topbar-mycredits-content-loggedinBefore" : "You have",
+	  "topbar-mycredits-content-loggedinAfter" : "Habbo Credits",
+	  "topbar-mycredits-content-notloggedinBefore" : "Please",
+	  "topbar-mycredits-content-notloggedinAfter" : "to see your balance",	  
+	  "topbar-mycredits-inner-buy" : "Buy More Coins",
+	  "topbar-mycredits-inner-redeem" : "Redeem a Coin or Furni Code",
+	  
+	  //TopBar -> My Club Tab
+	  
+	  "topbar-habboclub" : "Habbo Club",  
+	  "topbar-habboclub-content-loggedin" : " You are not a member of Habbo Club",
+	  "topbar-habboclub-content-loggedinBefore" : "You have",
+	  "topbar-habboclub-content-loggedinAfter" : "Days on Habbo Club",
+	  "topbar-habboclub-content-notloggedinBefore" : "Please",
+	  "topbar-habboclub-content-notloggedinAfter" : "to see your Club status",	
+	  "topbar-habboclub-inner-about": "Habbo Club gives you access to the full benefits on Habbo Hotel.",
+	  "topbar-habboclub-inner-club" : "Latest News on Habbo Club",
+ 
+
+  //Navbar
+  "navbar-home" : "Home",
+  "navbar-community" : "Community",  
+  "navbar-games" : "Games",    
+  "navbar-credits" : "Coins",    
+  "navbar-habboclub" : "Habbo Club",   
+  "navbar-groups" : "Groups",  
+  "navbar-help" : "Help",   
+
+  //News Box
+  "promoheader" : "Top Stories",
+  "newsbox-header" : "What's New",
+  "newsbox-footer" : "More News",
+
+  //Login
+  "login-headline-wrapperLeft" : "NEW TO HABBO? REGISTER HERE!",
+  "login-content-subheaderLeft" : "REGISTER NOW!",
+  "login-content-contentLeft" : "Habbo is an online community that lets you create your own virtual space for you and your friends. With thousands of members already checked in there's always something to do...", 
+  "login-content-registrationText": "PLEASE START BY ENTERING YOUR BIRTHDAY",
+  "login-content-registrationSubmit": "Continuar",  
+  "login-content-registrationReason": "TOP REASONS TO REGISTER",
+  "login-content-registerReason1": "Create your own Habbo character and Home page",
+  "login-content-registerReason2": "Meet your friends and find new ones",
+  "login-content-registerReason3": "Decorate your own room",
+  "login-content-registerReason4": "It’s more fun than not joining!",
+  "login-content-registerReason5": "It's completely free!",
+  
+  "login-headline-wrapperRight": "ALREADY HAVE A HABBO? PLEASE LOG IN HERE!",
+  "login-content-subheaderRight": " ",
+  "login-content-contentRight": "If you already have a Habbo account then log in here using your Habbo user name and your password. Your user name and password are the same for here as they are in the Hotel.",
+  "login-content-holderUser": "MY HABBO NAME",
+  "login-content-holderPassword": "PASSWORD",
+  "login-content-loginSubmit": "LOG IN",  
+  "login-content-loginForgotTitle": "FORGOTTEN YOUR PASSWORD?",   
+  "login-content-loginForgotContent": "If you have forgotten your password please contact Player Support by using the ",  
+  "login-content-loginForgotContentURL": "Habbo Help Tool",  
+  "login-content-loginSafetyTitle": "SECURITY INFORMATION",   
+  "login-content-loginSafetyContent": "The real Habbo site is encrypted to protect you and your data. You can check whether or not this site is encrypted by looking for the nice looking padlock at the bottom of your web browser.",
+  
+   
+   //Client
+   "topbar-client-count": "Habbos online now",
+   "topbar-client-logout": "CLOSE HOTEL",
+   
+   
+     //Register
+	 
+	"register-headerTitle": "Registration",
+	"register-headerExit": "Cancel",
+	"register-BubbleContent0": "Please tell us your birthday.",
+	"register-StepTitle-Birthday" : "Birthday",
+	"register-BubbleContent1": "Now the fun begins! Choose what you want to look like in habbo!",
+	"register-BubbleContent2a": "Now to pick your habbo name and password. A good password should contain numbers and both UPPER and lowercase letters.",
+	"register-StepTitle-Username" : "CHOOSE YOUR USERNAME: ",
+ 	"register-StepContent-UsernameBefore" :"Your",
+	"register-StepHolder-Username": "username", 
+ 	"register-StepContent-UsernameAfter" : "can contain lowercase and uppercase letters. Your username can also contain numbers and the following characters: -=?!@:. ",
+	"register-StepTitle-Password" : "NOW, CHOOSE YOUR PASSWORD:",
+ 	"register-StepContent-PasswordBefore" : "Your",
+	"register-StepHolder-Password": "password",  
+ 	"register-StepContent-PasswordAfter" :"must be at least six characters long. Your password can contain both uppercase letters and numbers.",
+	"register-StepHolder-RetypePassword": "Retype Password",	
+	"register-BubbleContent2b": "Now to pick your Habbo name! Who are you going to be in Habbo?",
+ 	"register-StepContent-UsernamePicked" : "Sorry, but that name is already taken. How about one of these ? ",
+	"register-StepHolder-Other": "Other:",  
+	"register-BubbleContent3": "Email is the only way for our Support team to contact you if you need any help with your account.",
+ 	"register-StepContent-Email" : "Please enter your email address.",
+	"register-StepHolder-Email": "Your email address:  ",  
+	"register-BubbleContent4": "Please read the Terms and Conditions carefully. These ones are more intresting than real habbo ones.",
+	"register-StepContent-TOS" : "You must agree to the following terms.",
+	"register-StepHolder-TOS" : "I accept the Terms and Conditions.",
+	"register-BubbleContent5Before": "Congratulations",
+	"register-BubbleContent5After": "! You are now a habbo!",
+ 	"register-StepTitle-Welcome" : "Welcome to Habbo!",
+	"register-Finish": "DONE",
+	"register-continue": "CONTINUE",
+	
+	//Logout 
+	"logout-headerTitle": "Logged out",
+	"logout-content1": "You have logged out succesfully.",
+	"logout-content2": "Goodbye, and we hope you come back soon!",
+	"logout-content3": "",
+	"logout-content4": "",
+	"logout-content4Url": "Sign in again",
+	
+	//Months
+	"month-1": "Janaury",
+	"month-2": "February",
+	"month-3": "March",
+	"month-4": "April",
+	"month-5": "May",
+	"month-6": "June",	
+	"month-7": "July",
+	"month-8": "August",
+	"month-9": "September",	
+	"month-10": "October",	
+	"month-11": "November",	
+	"month-12": "December",	
+};
