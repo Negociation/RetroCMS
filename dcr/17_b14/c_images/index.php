e8c4a4beb6fcfa807bd03e6dac21db19ee3fd1c9
Negociation <tiagitofranca04@gmail.com>
2019-01-06 18:55:50 -0200

    Fixed a bug with hcdays

--- dcr/17_b14/c_images/index.php
+++
@@ -1,3 +0,0 @@
-<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
-<title>...</title>
-<meta http-equiv="refresh" content="0; URL=http://wirbelwind.legtux.org/index.php">
