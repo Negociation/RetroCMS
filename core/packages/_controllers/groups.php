94ff85490f217932e0bb5b548a5b764ef0607999
Marcos <tiagitofranca04@gmail.com>
2019-04-20 22:14:36 -0300

    Added Habbo Group View and Ajax

--- core/packages/_controllers/groups.php
+++ core/packages/_controllers/groups.php
@@ -11,10 +11,10 @@
 // Compatibility Version(s): [r14,r15,r16,r17]				//
 //////////////////////////////////////////////////////////////
 
-// Class: Club
-// Desc: Habbo Club Controller 
+// Class: Groups
+// Desc: All Habbo Groups Related Content
 
-class Club extends ControllerTemplate{
+class Groups extends ControllerTemplate{
 	
 	/* Construct Method */
 	public function __construct($hotelConection){
@@ -24,80 +24,43 @@ class Club extends ControllerTemplate{
 		//Generic Models
 		$this->hotelModel = new hotelModel($this->hotelConection);
 		$this->habboModel = new habboModel($this->hotelConection);
-
+		
 		//Get Hotel Object
 		$this->hotel = $this->hotelModel->get_HotelObject();
 		
 		//New Habbo Object
 		$this->habbo = new Habbo();
-
 		//If Logged In
 		if($this->habboModel->get_SessionStatus($this->habbo->get_habboSession())){
 			$this->habbo = $this->habboModel->get_HabboObject($this->habbo->get_HabboId(),1);
 			$this->habbo->set_isHabboLoggedIn(true);
 		}else{
-			$this->habbo->set_isHabboLoggedIn(false);		
+			$this->habbo->set_isHabboLoggedIn(false);
 		}
-		
+
+
 	}
 	
-	/* Default View Call - Club */
+	/* Default View Call */
 	protected function default(){
 		//Set Page Title;
-		$this->pageTitle = "Habbo Club";
-		include 'web/club/index.view';	
+		$this->pageTitle = "Habbo Groups";
+		include 'web/groups/index.view';	
 		exit;
 	}
 	
-
-	/* View Call - Club/Join */
-	protected function join(){
-		//Set Page Title;
-		$this->pageTitle = "Habbo";
-		include 'web/club/join.view';	
-		exit;
-	}	
-	
-	
-	/* View Call - Club/Shop */
-	protected function shop(){
-		//Set Page Title;
-		$this->pageTitle = "Habbo";
-		include 'web/club/shop.view';	
-		exit;
-	}
-	
-	protected function club_subscribe(){
-		echo 'result';
-		
-	}
-	
-	
-	protected function club_subscribe_form(){
-		if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["optionNumber"])){
+	/* Ajax Create Group Form */
+	protected function group_create_form(){
+		if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["product"])){
 			if($this->habbo->get_isHabboLoggedIn()){
-				switch($_POST["optionNumber"]){
-					case 'subscribe1':
-						$optionDays = 31;
-						$optionCredits = 25;
-						break;
-					case 'subscribe2':
-						$optionDays = 96;
-						$optionCredits = 60;
-						break;
-					case 'subscribe3':
-						$optionDays = 186;
-						$optionCredits = 105;
-						break;							
-				}				
-				include './web/includes/modules/ajax/club_subscribe_form.ajax';
-				exit;		
+				include './web/includes/modules/ajax/group_create_form.ajax';
+				exit;
 			}else{
 				include './web/includes/modules/ajax/ajax_logged_required.ajax';
 				exit;
 			}
 		}else{
-			echo 'not allowed';
+			echo 'not allowed';	
 		}
 	}
 	
