584b1cb0d6a7f202e730743e0d7636e49afd3fd1
Negociation <32465944+Negociation@users.noreply.github.com>
2020-04-14 18:26:07 -0300

    Started the MVC Structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//////////////////////////////////////////////////////////////
+//						  RetroCMS							//
+//<<<<<<<<<<<<<< The Oldschool Era is Back >>>>>>>>>>>>>>>>>//
+//----------------------------------------------------------//
+// Developed by: Marcos [ aka: m.tiago ]					//
+// Development Thread: goo.gl/nwzdZo						//
+//////////////////////////////////////////////////////////////
+// Beta Version 0.9.5.110 ( Aquamarine ) 				    //
+// Branch: Public (Unstable)								//
+// Compatibility Version(s): [r14,r15,r16,r17]				//
+//////////////////////////////////////////////////////////////
+
+//UTF-8(Portuguese and Spanish Accents for PHP)
+header("Content-Type: text/html; charset=utf-8",true);
+
+
+// + Call the Configuration File Once
+// - File: Core.php
+// - Desc: Database Settings 
+
+
+require_once('./core/core.php');
+
+// + Start Session "habboWebsite"
+// - Desc: Sessions for Website
+session_start();	
+?>
