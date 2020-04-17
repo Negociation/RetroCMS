695945f030a57022fdc4017cbfd4c9dbadefe3bd
Negociation <32465944+Negociation@users.noreply.github.com>
2020-04-16 22:19:57 -0300

    Added Router Validation

--- application/appcore/_system/Router.php
+++ application/appcore/_system/Router.php
@@ -21,10 +21,54 @@ namespace System;
 
 final class Router{
 	
+	private $urlRequest;
+	private $urlParsed;
+	private $urlRoutes = [];
 	//Default Construct Method
 	public function __construct(){
-		echo 'oi';
+		
+		//Sanatize Request
+		if(isset($_SERVER["REQUEST_URI"])){
+			
+			//Sanatize Request String
+			$this->urlRequest = strtok(strtolower(trim( substr(substr($_SERVER["REQUEST_URI"],-1) == '/' ?  substr($_SERVER["REQUEST_URI"],0,-1) : $_SERVER["REQUEST_URI"],1))), '?');	
+
+			//Split String into Array
+			$this->urlParsed = (!empty($this->urlRequest))? explode("/",filter_var(rtrim($this->urlRequest), FILTER_SANITIZE_URL)) : $this->urlRequest;
+		}
+		
+		//Load URL
+			$this->loadRequest();
 	}
+	
+	//Handle request based on URL
+	private function handleRequest(){
+		if(is_array($this->urlParsed)){
+			$urlTarget = [$this->urlParsed[0],isset($this->urlParsed[1]) ? $this->urlParsed[1] : 'default', count($this->urlParsed) > 2 ? array_slice($this->urlParsed, 1) : null];
+			return $urlTarget;
+		}else{
+			return ['index','default',null];
+		}
+	}
+	
+	
+	//Load Request Controller based on URL
+	public function loadRequest(){
+		
+		//Handle Request based on Route Rules
+		$urlTarget = $this->handleRequest();
+		
+		call_user_func_array();
+
+		print_r($urlTarget);
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 	
 	
