99a23dda8cdf679976c34dccbeceae84a70b4708
Negociation <32465944+Negociation@users.noreply.github.com>
2020-04-29 23:00:22 -0300

    Fixed some Layout Stuff and Validations
    Fixed CSS for Disabled Nav, Fixed Translator for Hotel Nav, Added Hotel Status Validation for myHabbo Client url , Fixed a issue with home Visibility validation, added %url%/home view

--- app/core/_controllers/home.php
+++ app/core/_controllers/home.php
@@ -28,13 +28,13 @@ final class Home extends \Template\Controller{
 	}
 	
 	function default(){
-		echo 'RetroCMS ~ Habbo Home Index';
+		include 'web/home/home.view';
 	}
 	
 	function loadById($param){
 		if(is_numeric($param) && $this->homeUser = $this->habboModel->get_HabboObject($param,1)){
 			if(true){
-				include 'web/home/'.(($this->homeUser->get_isHabboHomeVisible() && ($this->homeUser->get_HabboId() == $this->habbo->get_HabboId())) ? 'home_view' : 'home_hidden').'.view';	
+				include 'web/home/'.(($this->homeUser->get_isHabboHomeVisible() || ($this->homeUser->get_HabboId() == $this->habbo->get_HabboId())) ? 'home_view' : 'home_hidden').'.view';	
 			}else{
 				include 'web/home/home_banned.view';
 			}
@@ -46,7 +46,7 @@ final class Home extends \Template\Controller{
 	function loadByName($param){
 		if($this->homeUser = $this->habboModel->get_HabboObject($param,2)){
 			if(true){
-				include 'web/home/'.(($this->homeUser->get_isHabboHomeVisible() && ($this->homeUser->get_HabboId() == $this->habbo->get_HabboId())) ? 'home_view' : 'home_hidden').'.view';	
+				include 'web/home/'.(($this->homeUser->get_isHabboHomeVisible() || ($this->homeUser->get_HabboId() == $this->habbo->get_HabboId())) ? 'home_view' : 'home_hidden').'.view';	
 			}else{
 				include 'web/home/home_banned.view';
 			}
